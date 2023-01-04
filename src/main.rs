4e4c5eaa3b4376126c54d88c2f8f9489de47a985
Noel Espino Córdova <github@ednoesco.com>
2023-01-04 11:22:09 -0600

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use actix_web::{get,  Responder, HttpResponse, App, HttpServer};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
